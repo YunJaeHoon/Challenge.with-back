a7c5179570e201df25122d5e9ae4766e9c118b1e
YunJaeHoon <sleepzzz214@naver.com>
2025-03-08 23:40:18 +0900

    feat: CORS 설정 추가

---
+++ src/main/java/Challenge/with_back/configuration/WebConfig.java
@@ -0,0 +1,20 @@
+package Challenge.with_back.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// CORS 설정 클래스
+@Configuration
+public class WebConfig implements WebMvcConfigurer
+{
+    @Override
+    public void addCorsMappings(CorsRegistry registry)
+    {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+    }
+}
