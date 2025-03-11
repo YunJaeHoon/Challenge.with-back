5dbaf1525408bf3fc687efc24fa0a14fed59a95b
YunJaeHoon <sleepzzz214@naver.com>
2025-03-11 17:03:29 +0900

    fix: 백엔드 서버 연결 테스트 api가 버전도 반환하도록 변경

--- src/main/java/Challenge/with_back/controller/BasicController.java
+++ src/main/java/Challenge/with_back/controller/BasicController.java
@@ -1,6 +1,7 @@
 package Challenge.with_back.controller;
 
 import Challenge.with_back.dto.response.SuccessResponseDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +14,12 @@ public class BasicController
 {
     // 백엔드 서버 연결 테스트
     @GetMapping("/check-connection")
-    public ResponseEntity<SuccessResponseDto> checkConnection()
+    public ResponseEntity<SuccessResponseDto> checkConnection(@Value("${spring.application.version}") String version)
     {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(SuccessResponseDto.builder()
                         .message("백엔드 서버와 연결을 성공적으로 완료하였습니다.")
-                        .data(null)
+                        .data(version)
                         .build());
     }
 }
