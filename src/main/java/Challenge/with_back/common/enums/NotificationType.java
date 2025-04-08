246614dd9a79b370e0f6cf05cc6a0a26373748d5
YunJaeHoon <sleepzzz214@naver.com>
2025-04-08 20:33:09 +0900

    fix: 알림과 친구 요청을 완전히 분리

---
+++ src/main/java/Challenge/with_back/common/enums/NotificationType.java
@@ -0,0 +1,13 @@
+package Challenge.with_back.common.enums;
+
+import lombok.AllArgsConstructor;
+
+// 알림 타입 enum
+@AllArgsConstructor
+public enum NotificationType
+{
+    WELCOME("회원가입 환영 알림"),
+    FRIEND_REQUEST("친구 요청 알림");
+
+    private final String description;
+}
