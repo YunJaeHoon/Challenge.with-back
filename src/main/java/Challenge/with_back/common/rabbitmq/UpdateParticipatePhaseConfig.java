68485f9627326d5bf816c1a0ed8f1826e86205b6
YunJaeHoon <sleepzzz214@naver.com>
2025-05-17 01:21:36 +0900

    fix: RabbitMQ 기본 설정과 페이즈 참여 정보 수정 큐 설정 클래스를 분리

--- src/main/java/Challenge/with_back/common/rabbitmq/UpdateParticipatePhaseConfig.java
+++ src/main/java/Challenge/with_back/common/rabbitmq/UpdateParticipatePhaseConfig.java
@@ -14,18 +14,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class UpdateParticipatePhaseConfig
 {
-    @Value("${RABBITMQ_HOST}")
-    private String host;
-
-    @Value("${RABBITMQ_PORT}")
-    private int port;
-
-    @Value("${RABBITMQ_USERNAME}")
-    private String username;
-
-    @Value("${RABBITMQ_PASSWORD}")
-    private String password;
-
     @Value("${RABBITMQ_UPDATE_PARTICIPATE_PHASE_EXCHANGE_NAME}")
     private String exchangeName;
 
@@ -89,53 +77,5 @@ public class UpdateParticipatePhaseConfig
                 .bind(updateParticipatePhaseDLQ())
                 .to(updateParticipatePhaseDLX())
                 .with(deadLetterRoutingKey);
-    }
-
-    // RabbitMQ 연결을 위한 ConnectionFactory 빈 생성
-    @Bean
-    public ConnectionFactory connectionFactory()
-    {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-
-        connectionFactory.setHost(host);
-        connectionFactory.setPort(port);
-        connectionFactory.setUsername(username);
-        connectionFactory.setPassword(password);
-
-        return connectionFactory;
-    }
-
-    // RabbitTemplate 빈 생성
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory)
-    {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-
-        // JSON 형식의 메시지와 객체 간의 직렬화 및 역직렬화가 가능하도록 설정
-        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
-
-        return rabbitTemplate;
-    }
-
-    // Jackson 라이브러리를 통해 메시지를 JSON 형식으로 변환하는 MessageConverter 빈 생성
-    @Bean
-    public MessageConverter jackson2JsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
-
-    // 재시도 정책 비활성화
-    @Bean
-    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory)
-    {
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-
-        // 예외 발생 시 메시지를 큐에 다시 넣지 않음
-        factory.setDefaultRequeueRejected(false);
-
-        // JSON 형식의 메시지와 객체 간의 직렬화 및 역직렬화가 가능하도록 설정
-        factory.setMessageConverter(jackson2JsonMessageConverter());
-
-        return factory;
     }
 }
