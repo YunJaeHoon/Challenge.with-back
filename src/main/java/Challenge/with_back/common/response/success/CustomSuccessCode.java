980f72950f27e3efd559a7585a0c01e93313fe10
YunJaeHoon <sleepzzz214@naver.com>
2025-05-26 11:39:30 +0900

    fix: 로그인 시, access token도 쿠키로 응답하도록 변경

--- src/main/java/Challenge/with_back/common/response/success/CustomSuccessCode.java
+++ src/main/java/Challenge/with_back/common/response/success/CustomSuccessCode.java
@@ -7,7 +7,11 @@ import lombok.Getter;
 @Getter
 public enum CustomSuccessCode
 {
-    SUCCESS("응답 분기가 없는 요청에 대한 성공 응답");
+    SUCCESS("응답 분기가 없는 요청에 대한 성공 응답"),
+
+    // 로그인
+    SUCCESS_REMEMBER("remember-me 체크를 했을 때의 성공"),
+    SUCCESS_FORGET("remember-me 체크를 하지 않았을 때의 성공");
 
     private final String message;
 }
--- src/main/java/Challenge/with_back/common/security/jwt/Token.java
+++ src/main/java/Challenge/with_back/common/security/jwt/Token.java
@@ -8,9 +8,10 @@ import lombok.Getter;
 @Getter
 public enum Token
 {
-    ACCESS_TOKEN("accessToken", 60 * 60 * 24),          // 1일
-    REFRESH_TOKEN("refreshToken", 60 * 60 * 24 * 30);   // 30일
+    ACCESS_TOKEN("accessToken", 60 * 60 * 24, false),          // 1일
+    REFRESH_TOKEN("refreshToken", 60 * 60 * 24 * 30, true);   // 30일
 
     private final String name;
     private final int validTime;
+    private boolean isHttpOnly;
 }
