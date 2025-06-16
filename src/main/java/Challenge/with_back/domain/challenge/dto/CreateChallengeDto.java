92ede25ea9a35676abb421f5e31af2202b987279
YunJaeHoon <sleepzzz214@naver.com>
2025-06-16 12:57:06 +0900

    fix: 챌린지 생성 시, 챌린지 설명은 필수가 아니도록 수정

--- src/main/java/Challenge/with_back/domain/challenge/dto/CreateChallengeDto.java
+++ src/main/java/Challenge/with_back/domain/challenge/dto/CreateChallengeDto.java
@@ -20,7 +20,6 @@ public class CreateChallengeDto
 	@Size(max = 255, message = "챌린지 이름은 최대 255자까지 입력할 수 있습니다.")
 	private String name;
 
-	@NotNull(message = "챌린지 설명을 입력해주세요.")
 	@Size(max = 255, message = "챌린지 설명은 최대 255자까지 입력할 수 있습니다.")
 	private String description;
 
